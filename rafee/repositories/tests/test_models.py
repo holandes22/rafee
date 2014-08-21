1fc4e878cfa696206653eca1176f7e29eb421c27
Pablo Klijnjan <pablokl@il.ibm.com>
2014-08-21 17:56:25 +0300

    Added unittests for the Repository model

---
+++ rafee/repositories/tests/test_models.py
@@ -0,0 +1,16 @@
+from django.test import TestCase
+from django.db import IntegrityError
+
+from rafee.repositories.models import GitRepository
+
+
+class RepositoryModelTests(TestCase):
+
+    def test_create_raises_validation_error_if_url_not_unique(self):
+        same_url = 'http://1'
+        repo1 = GitRepository(url=same_url)
+        repo2 = GitRepository(url=same_url)
+        repo1.save()
+        with self.assertRaises(IntegrityError) as ctx:
+            repo2.save()
+            self.assertTrue('url' in str(ctx.exception))
