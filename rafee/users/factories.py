7417d8af135e373ce217b31de23c37e679c2ecef
Pablo Klijnjan <pabloklijnjan@gmail.com>
2014-08-28 15:45:33 +0300

    Modified user to have the email as username
        - User Django's AbstractBaseUser instead of
          AbstractUser to achieve this

--- rafee/users/factories.py
+++ rafee/users/factories.py
@@ -9,18 +9,8 @@ class UserFactory(DjangoModelFactory):
 
     class Meta:
         model = settings.AUTH_USER_MODEL
-        django_get_or_create = ('username',)
+        django_get_or_create = ('email',)
 
     team = factory.SubFactory(TeamFactory)
-    username = 'tata'
-    first_name = 'Gerardo'
-    last_name = 'Martino'
     full_name = 'Gerardo Daniel Martino'
-    is_staff = False
-    is_superuser = False
-    email = factory.LazyAttribute(
-        lambda a: '{}.{}@example.com'.format(
-            a.first_name,
-            a.last_name
-        ).lower()
-    )
+    email = factory.Sequence(lambda x: 'tata.martino{}@nob.com'.format(x))
--- rafee/users/models.py
+++ rafee/users/models.py
@@ -1,10 +1,25 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 
 from rafee.teams.models import Team
 
 
-class User(AbstractUser):
+class User(AbstractBaseUser, PermissionsMixin):
 
+    email = models.EmailField(verbose_name='email address', unique=True)
     full_name = models.CharField(max_length=200, default='')
     team = models.ForeignKey(Team, related_name='users')
+    is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
+
+    USERNAME_FIELD = 'email'
+
+    def get_full_name(self):
+        return self.full_name
+
+    def get_short_name(self):
+        return self.email
+
+    @property
+    def is_staff(self):
+        return self.is_admin
