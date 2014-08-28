d724f279a1128c21e6b55895aceda99fbf8c0055
Pablo Klijnjan <pabloklijnjan@gmail.com>
2014-08-28 15:43:39 +0300

    Ensuring PYTHONPATH in runtest script

--- runtests.sh
+++ runtests.sh
@@ -3,6 +3,7 @@ if [ -z "$VIRTUAL_ENV" ]; then
     exit 1
 fi
 
+export PYTHONPATH=$PYTHONPATH:./rafee
 find . -name "*.pyc" -exec rm -rf {} \;
 export DJANGO_SETTINGS_MODULE=rafee.settings.test
 python manage.py makemigrations && python manage.py migrate && coverage run --source='.' manage.py test
