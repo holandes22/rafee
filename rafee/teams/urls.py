3bf7e57a7110aed6dc3f79a767b108fa61de6b43
Pablo Klijnjan <pablokl@il.ibm.com>
2014-09-07 10:44:54 +0300

    PEP8

--- rafee/teams/urls.py
+++ rafee/teams/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import patterns, url
 
 from rafee.teams.views import TeamListAPIView, TeamDetailAPIView
 
+
 urlpatterns = patterns(
     'rafee.teams',
     url(r'^$', TeamListAPIView.as_view(), name='team-list'),
--- rafee/users/urls.py
+++ rafee/users/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import patterns, url
 
 from rafee.users.views import UserListAPIView, UserDetailAPIView
 
+
 urlpatterns = patterns(
     'rafee.users',
     url(r'^$', UserListAPIView.as_view(), name='user-list'),
