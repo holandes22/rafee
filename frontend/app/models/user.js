b54324f9adc8a53f1f31187ee2df459894d95699
Pablo Klijnjan <pabloklijnjan@gmail.com>
2014-09-26 19:21:36 +0300

    [frontend] Adding models for teams and users

--- frontend/app/models/user.js
+++ frontend/app/models/user.js
@@ -1,7 +1,9 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
+    username: DS.attr('string'),
     email: DS.attr('string'),
     fullName: DS.attr('string'),
     isStaff: DS.attr('boolean'),
+    teams: DS.hasMany('team', { async: true })
 });
