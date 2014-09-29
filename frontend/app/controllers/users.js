c98ae5bd7477e5a71620b798008a3507e2ef522f
Pablo Klijnjan <pabloklijnjan@gmail.com>
2014-09-29 20:07:12 +0300

    [frontend] Added search by name in users

--- frontend/app/controllers/users.js
+++ frontend/app/controllers/users.js
@@ -4,5 +4,21 @@ export default Ember.ArrayController.extend({
 
   sortProperties: ['fullName'],
   sortAscending: true,
+  searchText: null,
 
+  searchResults: function() {
+    var searchText = this.get('searchText');
+    var users = this.get('arrangedContent');
+    if (!searchText) {
+      return users;
+    }
+    return users.filter(function(user) {
+      var fullName = user.get('fullName');
+      if (fullName) {
+        return user.get('fullName').match(searchText);
+      }
+      return false;
+    });
+  }.property('arrangedContent', 'searchText')
 });
+
