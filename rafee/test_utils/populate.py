beeb06a04399242ee45d5f0af1d5078a1193e86b
Pablo Klijnjan <pabloklijnjan@gmail.com>
2014-09-30 22:22:23 +0300

    [backend] Minor improvements to the populate script for tests

--- rafee/test_utils/populate.py
+++ rafee/test_utils/populate.py
@@ -1,22 +1,36 @@
 import os
+import sys
+
+sys.path.append('/vagrant')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'rafee.settings.dev')
 
 import django
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'rafee.settings.dev')
-django.setup()
+django.setup()  # Call this before importing the models
 
+from rafee.teams.models import Team
 from rafee.users.factories import UserFactory
 from rafee.teams.factories import TeamFactory
 from rafee.slideshows.factories import SlideshowFactory
 from rafee.repositories.tasks import clone_and_create_repo
 
 
-team1 = TeamFactory(name='Team Fortress 2')
-team2 = TeamFactory(name='The A team')
+teams = []
+
+
+print 'Creating teams'
+for name in ['Team Fortress 2', 'The A team']:
+    try:
+        team = Team.objects.get(name=name)
+        teams.append(team)
+    except Team.DoesNotExist:
+        teams.append(TeamFactory(name=name))
+
+print 'Teams: ', teams
 
 admin = UserFactory(is_staff=True)
-user1 = UserFactory(teams=[team1])
-user2 = UserFactory(teams=[team1, team2])
+user1 = UserFactory(teams=(teams[0],))
+user2 = UserFactory(teams=teams)
 
 repo_urls = [
     'https://github.com/holandes22/hsg-templates.git',
@@ -29,7 +43,8 @@ for repo_url in repo_urls:
     result = task.get()
 
 templates1 = 'hsg-templates/commits,css-templates/users'
-slideshow1 = SlideshowFactory(templates=templates1, team=team1)
+slideshow1 = SlideshowFactory(templates=templates1, team=teams[0])
 
 templates2 = 'css-templates/user_repos,css-templates/users,css-templates/no_data'
-slideshow1 = SlideshowFactory(templates=templates2, team=team2)
+slideshow1 = SlideshowFactory(templates=templates2, team=teams[1])
+print 'Done'
