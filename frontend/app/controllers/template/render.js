a1035f14a7db9b24d37b2483308fcc947f095d10
Pablo Klijnjan <pabloklijnjan@gmail.com>
2014-10-04 13:09:42 +0300

    [frontend] Showing a spinner while template is rendering

--- frontend/app/controllers/template/render.js
+++ frontend/app/controllers/template/render.js
@@ -2,6 +2,15 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
   task: null,  // this is set from the route
+  isLoading: function() {
+    if (!this.task) {
+      return false;
+    }
+    if (this.task && this.task.get('status') !== 'PENDING') {
+      return false;
+    }
+    return true;
+  }.property('task.result'),
 
   renderResult: function() {
     if (this.task) {
