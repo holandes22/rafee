64ff03b00407c38883133638366eb55099a90059
Pablo Klijnjan <pabloklijnjan@gmail.com>
2014-10-07 11:01:04 +0300

    [frontend] Rearraging routes to obtain desired behavior
        - The initial design of nested routes was reflecting
          (as expected in ember) in the template rendering.
          We achieved the desired behaviour by re-ordering the
          routes

--- frontend/app/router.js
+++ frontend/app/router.js
@@ -5,18 +5,24 @@ var Router = Ember.Router.extend({
   location: config.locationType
 });
 
+// Altough it seems odd, we need to laid down delete
+// and edit routes next to the resource since we want
+// their templates to be rendere in the main resource
+// outlet. See a good explanation why in this blog post:
+// http://hashrocket.com/blog/posts/ember-routing-the-when-and-why-of-nesting
+//
 Router.map(function() {
   this.route('login');
   this.route('profile');
   this.route('presentation', { path: 'presentation/:slideshow_id' });
   this.route('admin', function() {
+    this.route('users/add');
     this.resource('users', function() {
-      this.route('add');
-      this.resource('user', { path: ':user_id' }, function() {
-        this.route('edit');
-        this.route('delete');
-      });
+      this.resource('user.delete', { path: ':user_id/delete' });
+      this.resource('user.edit', { path: ':user_id/edit' });
+      this.resource('user', { path: ':user_id' });
     });
+
     this.resource('teams', function() {
       this.route('add');
       this.resource('team', { path: ':team_id' }, function() {
