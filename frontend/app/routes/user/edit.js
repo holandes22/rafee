55a4aeb9ded4a068941bfb7b0cc12ff3f70e889c
Pablo Klijnjan <pabloklijnjan@gmail.com>
2014-11-18 18:45:20 +0200

    [frontend] Preventing dirty user model on route exit

--- frontend/app/routes/user/edit.js
+++ frontend/app/routes/user/edit.js
@@ -16,6 +16,21 @@ export default Ember.Route.extend({
       }, function(reason){
         window.console.log(reason);
       });
+    },
+    //TODO: put this in a mixin
+    willTransition: function(transition) {
+      var model = this.controller.get('model');
+      if (model.get('isDirty')) {
+          if (confirm("Are you sure you want to abandon progress?")) {
+            model.rollback();
+          } else {
+            transition.abort();
+          }
+      } else {
+        // Bubble the `willTransition` action so that
+        // parent routes can decide whether or not to abort.
+        return true;
+      }
     }
   }
 });
