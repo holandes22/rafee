330ee9277e1b7d5a04fa1f50d68a331286d540fc
Pablo Klijnjan <pabloklijnjan@gmail.com>
2014-11-18 16:10:11 +0200

    [frontend] Fixed missing 'cancel' action

--- frontend/app/controllers/user/delete.js
+++ frontend/app/controllers/user/delete.js
@@ -2,23 +2,17 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
 
-  needs: 'users',
-
   actions: {
     delete: function() {
       var self = this;
       this.get('model').destroyRecord().then(function() {
-        self.get('controllers.users').get('model').update();
-      }).then(function() {
-        // TODO: this is mighty ugly, but for some reason
-        // updating model is not trigerring a searchResult
-        self.get('controllers.users').set('searchText', '');
         self.transitionToRoute('users');
       });
+    },
+
+    cancel: function() {
+      this.transitionToRoute('users');
     }
   },
 
-  cancel: function() {
-    this.transitionToRoute('users');
-  }
 });
