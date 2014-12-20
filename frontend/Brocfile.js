f29435822e4e06424f5eed3f7cfccea59b2b3ea9
Pablo Klijnjan <pabloklijnjan@gmail.com>
2014-12-20 19:33:15 +0200

    [frontend] Using minified version of bootstrap-theme
        - Removes the annoying error when failing to
          retrieve the map file

--- frontend/Brocfile.js
+++ frontend/Brocfile.js
@@ -5,7 +5,7 @@ var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 var app = new EmberApp();
 
 app.import('bower_components/bootstrap/dist/css/bootstrap.css');
-app.import('bower_components/bootstrap/dist/css/bootstrap-theme.css');
+app.import('bower_components/bootstrap/dist/css/bootstrap-theme.min.css');
 app.import('bower_components/bootstrap/dist/fonts/glyphicons-halflings-regular.svg', { destDir: '/fonts'});
 app.import('bower_components/bootstrap/dist/fonts/glyphicons-halflings-regular.eot', { destDir: '/fonts'});
 app.import('bower_components/bootstrap/dist/fonts/glyphicons-halflings-regular.ttf', { destDir: '/fonts'});
