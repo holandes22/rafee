0730c7f631fbd8052f8eb0524955d50c212d77f0
Pablo Klijnjan <pabloklijnjan@gmail.com>
2015-01-10 21:03:32 +0200

    [frontend] Added parsePostData test helper

--- frontend/tests/acceptance/login-test.js
+++ frontend/tests/acceptance/login-test.js
@@ -1,24 +1,16 @@
 import Ember from 'ember';
 import Pretender from 'pretender';
 import startApp from '../helpers/start-app';
+import parsePostData from '../helpers/parse-post-data';
 import ENV from 'rafee/config/environment';
 
-var App;
+var application;
 var server;
-
-function parsePostData(query) {
-  var result = {};
-  query.split("&").forEach(function(part) {
-    var item = part.split("=");
-    result[item[0]] = decodeURIComponent(item[1]);
-  });
-  return result;
-}
 
 
 module('Integration - Login', {
   setup: function() {
-    App = startApp();
+    application = startApp();
     server = new Pretender(function() {
         this.post(ENV.APP.API_NAMESPACE + '/auth-token', function(request) {
             var data = parsePostData(request.requestBody);
@@ -31,21 +23,9 @@ module('Integration - Login', {
     });
   },
   teardown: function() {
-    Ember.run(App, 'destroy');
+    Ember.run(application, 'destroy');
     server.shutdown();
   }
-});
-
-test('It can login successfully', function() {
-  visit('/login');
-  fillIn('.login-username', 'isidoro');
-  fillIn('.login-password', 'good');
-  click('.form-signin button');
-
-  andThen(function() {
-    equal(find('.alert-success').length, 1, 'it shows the success messsage');
-    equal(find('.alert-danger').length, 0, 'it hides the failure messsage');
-  });
 });
 
 test('It shows error message on failed login', function() {
---
+++ frontend/tests/helpers/parse-post-data.js
@@ -0,0 +1,8 @@
+export default function parsePostData(query) {
+  var result = {};
+  query.split("&").forEach(function(part) {
+    var item = part.split("=");
+    result[item[0]] = decodeURIComponent(item[1]);
+  });
+  return result;
+}
