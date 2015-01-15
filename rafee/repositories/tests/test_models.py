12ff7fd7a048c44ed60808eb97be4625141b7db3
Pablo Klijnjan <pabloklijnjan@gmail.com>
2015-01-15 13:53:22 +0200

    [backend] Minor refactor

--- rafee/repositories/tests/test_models.py
+++ rafee/repositories/tests/test_models.py
@@ -8,9 +8,7 @@ class RepositoryModelTests(TestCase):
 
     def test_create_raises_validation_error_if_url_not_unique(self):
         same_url = 'http://1'
-        repo1 = Repository(url=same_url)
-        repo2 = Repository(url=same_url)
-        repo1.save()
+        Repository.objects.create(url=same_url)
         with self.assertRaises(IntegrityError) as ctx:
-            repo2.save()
+            Repository.objects.create(url=same_url)
             self.assertTrue('url' in str(ctx.exception))
