1bd322986838f4c77c0c7da66b3489f4fd216321
Pablo Klijnjan <pabloklijnjan@gmail.com>
2015-01-29 16:16:58 +0200

    [frontend] gh-50: Added auth route to profile

--- frontend/app/pods/profile/route.js
+++ frontend/app/pods/profile/route.js
@@ -1,8 +1,8 @@
 import Ember from 'ember';
+import AuthenticatedRouteMixin from 'simple-auth/mixins/authenticated-route-mixin';
 
-export default Ember.Route.extend({
-
-    model: function() {
-        return this.store.find('user', 'profile');
-    }
+export default Ember.Route.extend(AuthenticatedRouteMixin, {
+  model: function() {
+    return this.store.find('user', 'profile');
+  }
 });
