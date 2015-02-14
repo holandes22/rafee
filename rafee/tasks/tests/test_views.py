6c6afff264ba4663643e6375c0b6a55c869d5787
Pablo Klijnjan <pabloklijnjan@gmail.com>
2015-02-14 09:48:24 +0200

    [backend] gh-63: Migrating tasks tests to pytest

--- rafee/tasks/tests/test_views.py
+++ rafee/tasks/tests/test_views.py
@@ -1,51 +1,55 @@
+import pytest
 from mock import patch
 from celery import states
 from django.core.urlresolvers import reverse
 
-from rafee.test_utils.base import BaseAPITestCase
+from rafee.test_utils.assert_helpers import assert_200_and_items_equal
+
+# pylint: disable=invalid-name,redefined-outer-name
+
+task_id = 'aa-11'
 
 
-class TaskTests(BaseAPITestCase):
+@pytest.yield_fixture
+def result():
+    with patch('rafee.tasks.views.app.AsyncResult') as AsyncResult:
+        yield AsyncResult.return_value
 
-    def extra_setup(self):
-        # pylint: disable=attribute-defined-outside-init
-        self.ar_patcher = patch('rafee.tasks.views.app.AsyncResult')
-        self.ar_mock = self.ar_patcher.start()
-        self.result = self.ar_mock.return_value
-        self.task_id = 'aaa-b1b1'
 
-    def test_detail_pending_task(self):
-        self.result.status = states.PENDING
-        self.result.ready.return_value = False
-        expected = {'id': self.task_id, 'status': states.PENDING}
-        url = reverse('task-detail', kwargs={'task_id': self.task_id})
-        response = self.client.get(url)
-        self.assertResponse200AndItemsEqual(expected, response)
+def test_detail_pending_task(result, client):
+    result.status = states.PENDING
+    result.ready.return_value = False
+    expected = {'id': task_id, 'status': states.PENDING}
+    url = reverse('task-detail', kwargs={'task_id': task_id})
+    response = client.get(url)
+    assert_200_and_items_equal(expected, response)
 
-    def test_detail_failed_task(self):
-        self.result.status = states.FAILURE
-        error = ValueError('message')
-        traceback = 'fake_tb\njahsjhaks'
-        self.result.result = error
-        self.result.traceback = traceback
-        self.result.ready.return_value = True
-        self.result.failed.return_value = True
-        expected = {
-            'id': self.task_id,
-            'status': states.FAILURE,
-            'result': str(error),
-            'traceback': traceback,
-        }
-        url = reverse('task-detail', kwargs={'task_id': self.task_id})
-        response = self.client.get(url)
-        self.assertResponse200AndItemsEqual(expected, response)
 
-    def test_detail_success_task(self):
-        self.result.status = states.SUCCESS
-        self.result.result = 1
-        self.result.ready.return_value = True
-        self.result.failed.return_value = False
-        expected = {'id': self.task_id, 'status': states.SUCCESS, 'result': 1}
-        url = reverse('task-detail', kwargs={'task_id': self.task_id})
-        response = self.client.get(url)
-        self.assertResponse200AndItemsEqual(expected, response)
+def test_detail_failed_task(result, client):
+    result.status = states.FAILURE
+    error = ValueError('message')
+    traceback = 'fake_tb\njahsjhaks'
+    result.result = error
+    result.traceback = traceback
+    result.ready.return_value = True
+    result.failed.return_value = True
+    expected = {
+        'id': task_id,
+        'status': states.FAILURE,
+        'result': str(error),
+        'traceback': traceback,
+    }
+    url = reverse('task-detail', kwargs={'task_id': task_id})
+    response = client.get(url)
+    assert_200_and_items_equal(expected, response)
+
+
+def test_detail_success_task(result, client):
+    result.status = states.SUCCESS
+    result.result = 1
+    result.ready.return_value = True
+    result.failed.return_value = False
+    expected = {'id': task_id, 'status': states.SUCCESS, 'result': 1}
+    url = reverse('task-detail', kwargs={'task_id': task_id})
+    response = client.get(url)
+    assert_200_and_items_equal(expected, response)
