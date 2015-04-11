dedc1a2a200b3127acfac25748fec43b17118b82
Pablo Klijnjan <pabloklijnjan@gmail.com>
2015-04-11 11:54:12 +0300

    [frontend] gh-71: Fixed bug when retrieving form field error

--- frontend/app/components/form-field.js
+++ frontend/app/components/form-field.js
@@ -2,11 +2,10 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
 
-  errorMessages: Ember.computed('errors', 'key', function() {
+  errorMessages: Ember.computed('errors', function() {
     var errors = this.get('errors');
-    if (errors) {
-      var key = this.get('key');
-      return errors[key].join(' ');
+    if (errors && errors.hasOwnProperty(this.key)) {
+      return errors[this.key].join(' ');
     }
     return '';
   })
