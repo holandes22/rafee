216b0986387463f7127969ef46eb050af4057441
Pablo Klijnjan <pabloklijnjan@gmail.com>
2015-04-14 20:25:32 +0300

    [backend] trivial: Fixed validation in API of User.email
        - Empty string is not allowed anymore

--- rafee/users/serializers.py
+++ rafee/users/serializers.py
@@ -1,9 +1,11 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import ModelSerializer, EmailField
 
 from rafee.users.models import User
 
 
 class UserSerializer(ModelSerializer):
+
+    email = EmailField(allow_blank=False)
 
     class Meta:
         model = User
